573d5eefa8d1ae87249a927a3e3d3041da2a1976
Izzatilla <112051978+1zzat1lla-Fayzullayev@users.noreply.github.com>
2024-05-12 09:47:33 +0500

    change admin panel

--- src/admin/Admin.jsx
+++ src/admin/Admin.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import supabase from '../supabase/data'
 import PicturesData from '../PicturesData'
+import { Link } from 'react-router-dom'
 
 function Admin() {
 	const [isLoggedIn, setIsLoggedIn] = useState(
@@ -51,12 +52,17 @@ function Admin() {
 		})
 	}
 
-	const handleOpenModal = () => {
-		document.getElementById('my_modal_1').showModal()
+	const handleSubmitCard = async e => {
+		e.preventDefault()
+		// Similar logic for card submission
 	}
 
-	const handleCloseModal = () => {
-		const modal = document.getElementById('my_modal_1')
+	const handleOpenModal = tab => {
+		document.getElementById(`my_modal_${tab}`).showModal()
+	}
+
+	const handleCloseModal = tab => {
+		const modal = document.getElementById(`my_modal_${tab}`)
 		if (modal) {
 			modal.close()
 		}
@@ -70,60 +76,219 @@ function Admin() {
 		<div className='absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]'>
 			{isLoggedIn ? (
 				<>
-					<div className='flex flex-col md:flex-row items-start justify-between'>
-						<div className='w-full md:w-[400px] h-[300px] md:h-[100vh] navbar_anima'>
-							<h1 className='font-bold cursor-pointer text-center text-[25px] text-white'>
-								Anime <span className='slider_h1 font-bold'>DUB</span>
-							</h1>
-						</div>
-						{/*  */}
-						<dialog id='my_modal_1' className='modal font-Inter'>
-							<div className='modal-box form_admin'>
-								<h3 className='font-bold text-[25px] my-[20px] text-center text-white'>
-									Swiper Form
-								</h3>
-								<form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-									<input
-										type='text'
-										name='title'
-										placeholder='Title'
-										className='input bg-[#17171A] text-white'
-										value={title}
-										onChange={handleChange}
-									/>
-									<input
-										type='text'
-										name='descreption'
-										placeholder='Description'
-										className='input bg-[#17171A] text-white'
-										value={descreption}
-										onChange={handleChange}
-									/>
-									<input
-										type='text'
-										name='picture'
-										placeholder='Picture URL'
-										className='input bg-[#17171A] text-white'
-										value={picture}
-										onChange={handleChange}
-									/>
-									<button className='btn btn-success text-white'>Submit</button>
-								</form>
-								<img
-									src={PicturesData.close}
-									alt='close'
-									onClick={handleCloseModal}
-									className='absolute top-2 right-2 w-[30px] cursor-pointer'
-								/>
+					<div className='flex justify-start flex-col md:flex-row items-start'>
+						<div className='w-full md:w-[360px] md:h-screen flex flex-col items-center md:pb-0 pb-[20px] pt-5 shadow-admin'>
+							<Link to={'/'}>
+								<h1 className='font-bold cursor-pointer text-white text-[25px]'>
+									Anime <span className='slider_h1 font-bold'>DUB</span>
+								</h1>
+							</Link>
+							<div className='mt-[35px] flex flex-col items-start gap-3 w-full px-6'>
+								<p
+									onClick={() => setTab(1)}
+									className={`${
+										tab == 1
+											? 'bg-[#458FF6] text-[#fff] font-medium '
+											: 'bg-[#ececec50] text-[white] '
+									} text-lg rounded-[8px] hover:cursor-pointer hover:translate-x-1.5 transition-all py-[8px] px-[25px] w-full mr-5`}
+								>
+									Swiper
+								</p>
+								<p
+									onClick={() => setTab(2)}
+									className={`${
+										tab == 2
+											? 'bg-[#458FF6] text-[#fff] font-medium '
+											: 'bg-[#ececec50] text-[white] '
+									} text-lg rounded-[8px] hover:cursor-pointer hover:translate-x-1.5 transition-all py-[8px] px-[25px] w-full mr-5`}
+								>
+									Card
+								</p>
+								<p
+									onClick={() => setTab(3)}
+									className={`${
+										tab == 3
+											? 'bg-[#458FF6] text-[#fff] font-medium '
+											: 'bg-[#ececec50] text-[white] '
+									} text-lg rounded-[8px] hover:cursor-pointer hover:translate-x-1.5 transition-all py-[8px] px-[25px] w-full mr-5`}
+								>
+									nmadrde yana
+								</p>
 							</div>
-						</dialog>
-
-						<button
-							className='btn btn-primary font-Inter mr-[20px] mt-[20px]'
-							onClick={handleOpenModal}
-						>
-							SwiperForm
-						</button>
+						</div>
+						<div className='p-5 w-full md:w-[calc(100%-360px)] min-h-screen overflow-auto'>
+							{tab === 1 && (
+								<div className='container mx-auto'>
+									<div className='flex justify-end items-end'>
+										<button
+											className='btn bg-[#458FF6] border-[#458FF6] text-white hover:bg-[#458FF6] font-Inter mr-[20px] mt-[20px]'
+											onClick={() => handleOpenModal(tab)}
+										>
+											SwiperForm
+										</button>
+									</div>
+									<dialog id={`my_modal_${tab}`} className='modal font-Inter'>
+										<div className='modal-box form_admin'>
+											<h3 className='font-bold text-[25px] my-[20px] text-center text-white'>
+												Swiper Form
+											</h3>
+											<form
+												onSubmit={handleSubmit}
+												className='flex flex-col gap-4'
+											>
+												<input
+													type='text'
+													name='title'
+													placeholder='Title'
+													className='input bg-[#17171A] text-white'
+													value={title}
+													onChange={handleChange}
+												/>
+												<input
+													type='text'
+													name='descreption'
+													placeholder='Description'
+													className='input bg-[#17171A] text-white'
+													value={descreption}
+													onChange={handleChange}
+												/>
+												<input
+													type='text'
+													name='picture'
+													placeholder='Picture URL'
+													className='input bg-[#17171A] text-white'
+													value={picture}
+													onChange={handleChange}
+												/>
+												<button className='btn btn-success text-white'>
+													Submit
+												</button>
+											</form>
+											<img
+												src={PicturesData.close}
+												alt='close'
+												onClick={() => handleCloseModal(tab)}
+												className='absolute top-2 right-2 w-[30px] cursor-pointer'
+											/>
+										</div>
+									</dialog>
+								</div>
+							)}
+							{tab === 2 && (
+								<div className='container mx-auto'>
+									<div className='flex justify-end items-end'>
+										<button
+											className='btn bg-[#458FF6] border-[#458FF6] text-white hover:bg-[#458FF6] font-Inter mr-[20px] mt-[20px]'
+											onClick={() => handleOpenModal(tab)}
+										>
+											CardForm
+										</button>
+									</div>
+									<dialog id={`my_modal_${tab}`} className='modal font-Inter'>
+										<div className='modal-box form_admin'>
+											<h3 className='font-bold text-[25px] my-[20px] text-center text-white'>
+												Card Form
+											</h3>
+											<form
+												onSubmit={handleSubmit}
+												className='flex flex-col gap-4'
+											>
+												<input
+													type='text'
+													name='title'
+													placeholder='Title'
+													className='input bg-[#17171A] text-white'
+													value={title}
+													onChange={handleChange}
+												/>
+												<input
+													type='text'
+													name='descreption'
+													placeholder='Description'
+													className='input bg-[#17171A] text-white'
+													value={descreption}
+													onChange={handleChange}
+												/>
+												<input
+													type='text'
+													name='picture'
+													placeholder='Picture URL'
+													className='input bg-[#17171A] text-white'
+													value={picture}
+													onChange={handleChange}
+												/>
+												<button className='btn btn-success text-white'>
+													Submit
+												</button>
+											</form>
+											<img
+												src={PicturesData.close}
+												alt='close'
+												onClick={() => handleCloseModal(tab)}
+												className='absolute top-2 right-2 w-[30px] cursor-pointer'
+											/>
+										</div>
+									</dialog>
+								</div>
+							)}
+							{tab === 3 && (
+								<div className='container mx-auto'>
+									<div className='flex justify-end items-end'>
+										<button
+											className='btn bg-[#458FF6] border-[#458FF6] text-white hover:bg-[#458FF6] font-Inter mr-[20px] mt-[20px]'
+											onClick={() => handleOpenModal(tab)}
+										>
+											NmadrForm
+										</button>
+									</div>
+									<dialog id={`my_modal_${tab}`} className='modal font-Inter'>
+										<div className='modal-box form_admin'>
+											<h3 className='font-bold text-[25px] my-[20px] text-center text-white'>
+												Nmadr Form
+											</h3>
+											<form
+												onSubmit={handleSubmit}
+												className='flex flex-col gap-4'
+											>
+												<input
+													type='text'
+													name='title'
+													placeholder='Title'
+													className='input bg-[#17171A] text-white'
+													value={title}
+													onChange={handleChange}
+												/>
+												<input
+													type='text'
+													name='descreption'
+													placeholder='Description'
+													className='input bg-[#17171A] text-white'
+													value={descreption}
+													onChange={handleChange}
+												/>
+												<input
+													type='text'
+													name='picture'
+													placeholder='Picture URL'
+													className='input bg-[#17171A] text-white'
+													value={picture}
+													onChange={handleChange}
+												/>
+												<button className='btn btn-success text-white'>
+													Submit
+												</button>
+											</form>
+											<img
+												src={PicturesData.close}
+												alt='close'
+												onClick={() => handleCloseModal(tab)}
+												className='absolute top-2 right-2 w-[30px] cursor-pointer'
+											/>
+										</div>
+									</dialog>
+								</div>
+							)}
+						</div>
 					</div>
 				</>
 			) : (
