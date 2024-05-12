5deb98b880b53c37dfdf905866cfd5bad68f3594
Izzatilla <112051978+1zzat1lla-Fayzullayev@users.noreply.github.com>
2024-05-12 15:48:42 +0500

    change

--- src/shared/MobileNavbar.jsx
+++ src/shared/MobileNavbar.jsx
@@ -4,14 +4,14 @@ import PicturesData from '../PicturesData'
 function MobileNavbar({ closeNavbar }) {
 	return (
 		<>
-			<div className='bg-[#ffffff] fixed top-0 left-0 bottom-0 right-0 w-full h-[100vh] rounded-[10px]'>
+			<div className='h-screen w-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] fixed top-0 left-0 bottom-0 right-0 rounded-[10px]'>
 				<img
 					src={PicturesData.close}
 					alt='close'
 					className='w-[40px] absolute right-2 top-2'
 					onClick={closeNavbar}
 				/>
-				<ul className='flex flex-col h-full justify-center items-center gap-[20px]'>
+				<ul className='flex flex-col h-full justify-center items-center gap-[20px] text-white'>
 					<li>
 						<a href='#'>Home</a>
 					</li>
