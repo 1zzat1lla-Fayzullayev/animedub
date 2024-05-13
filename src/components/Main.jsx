89631c69cdac675d280fefcf5d5d8144b9936086
Izzatilla <112051978+1zzat1lla-Fayzullayev@users.noreply.github.com>
2024-05-13 21:49:25 +0500

    change

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -11,7 +11,7 @@ function Main() {
 			<Wrapper>
 				<div className='flex gap-2 justify-center md:justify-start items-center'>
 					<h2 className='linear_text text-[30px] font-bold font-Montserrat my-[20px]'>
-						Trending
+						Popular
 					</h2>
 					<img src={PicturesData.star} alt='star' className='w-[30px]' />
 				</div>
