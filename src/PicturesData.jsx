da5c71744e54787d24f157aa6657a3f9c773c456
Izzatilla <112051978+1zzat1lla-Fayzullayev@users.noreply.github.com>
2024-06-07 22:59:35 +0500

    change jpg to png

--- src/PicturesData.jsx
+++ src/PicturesData.jsx
@@ -5,6 +5,6 @@ import user from './assets/user.png'
 import premium from './assets/premium.png'
 import lock from './assets/lock.png'
 import search from './assets/search.png'
-import logo from './assets/logo.jpg'
+import logo from './assets/logo.png'
 
 export default { close, play, star, user, premium, lock, search, logo }
