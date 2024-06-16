1b8b8e11807b5b1d27956de98cd50c29734d486e
Izzatilla <112051978+1zzat1lla-Fayzullayev@users.noreply.github.com>
2024-06-16 12:59:43 +0500

    change state

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -28,18 +28,18 @@ function Main({ user, imagesLoaded }) {
 				<SliderCard user={user} />
 				<div className='flex gap-2 justify-center md:justify-start items-center'>
 					<h2 className='linear_text text-[30px] font-bold font-Poppins my-[20px]'>
-						{premiumText}
-					</h2>
-					<img src={PicturesData.star} alt='star' className='w-[30px]' />
-				</div>
-				<SliderPremiumCard user={user} />
-				<div className='flex gap-2 justify-center md:justify-start items-center'>
-					<h2 className='linear_text text-[30px] font-bold font-Poppins my-[20px]'>
 						Seriallar
 					</h2>
 					<img src={PicturesData.star} alt='star' className='w-[30px]' />
 				</div>
 				<DoramaCard user={user} />
+				<div className='flex gap-2 justify-center md:justify-start items-center'>
+					<h2 className='linear_text text-[30px] font-bold font-Poppins my-[20px]'>
+						{premiumText}
+					</h2>
+					<img src={PicturesData.star} alt='star' className='w-[30px]' />
+				</div>
+				<SliderPremiumCard user={user} />
 			</Wrapper>
 			<Card user={user} />
 		</div>
